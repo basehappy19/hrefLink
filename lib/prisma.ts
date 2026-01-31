7d6a70ff7b8e3ad090795b4aa39f5ebc186eb304
DESKTOP-EL7LBE9 <pakpoom.td@gmail.com>
2026-01-31 08:08:25 +0700

    ret

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/client";
+
 const adapter = new PrismaMariaDb({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
